700919df243625da43f6079e56dae1fa2a664691
xia0hj <719887163@qq.com>
2023-01-29 20:51:46 +0800

    build: windows-rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,40 @@
     windows_subsystem = "windows"
 )]
 
+use windows::{
+    Win32::Foundation::{BOOL, HWND, LPARAM},
+    Win32::UI::WindowsAndMessaging::{
+        EnumWindows, GetWindowInfo, GetWindowTextW, WINDOWINFO, WS_VISIBLE,
+    },
+};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    unsafe { 
+        let res = EnumWindows(Some(enum_window), LPARAM(0)).ok();
+    }
+    return format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+extern "system" fn enum_window(window: HWND, _: LPARAM) -> BOOL {
+    unsafe {
+        let mut text: [u16; 512] = [0; 512];
+        let len = GetWindowTextW(window, &mut text);
+        let text = String::from_utf16_lossy(&text[..len as usize]);
+
+        let mut info = WINDOWINFO {
+            cbSize: core::mem::size_of::<WINDOWINFO>() as u32,
+            ..Default::default()
+        };
+        GetWindowInfo(window, &mut info).unwrap();
+
+        if !text.is_empty() && info.dwStyle & WS_VISIBLE.0 != 0 {
+            println!("{} ({}, {})", text, info.rcWindow.left, info.rcWindow.top);
+        }
+
+        true.into()
+    }
 }
 
 fn main() {
